26943eccf0e2d18b6770518925675c69da459a8f
liyanjie <liyanjie@cloudwalk.cn>
2020-05-23 15:55:05 +0800

    netty服务端和客户端通信

--- netty/src/main/java/com/example/netty/client/FirstClientHandler.java
+++ netty/src/main/java/com/example/netty/client/FirstClientHandler.java
@@ -8,28 +8,32 @@ import java.nio.charset.Charset;
 import java.util.Date;
 
 public class FirstClientHandler extends ChannelInboundHandlerAdapter {
-
+    @Override
     public void channelActive(ChannelHandlerContext ctx) {
         System.out.println(new Date() + ": 客户端写出数据");
 
-        // 获取数据
+        // 1.获取数据
         ByteBuf buffer = getByteBuf(ctx);
 
-        // 写数据
-        ctx.channel().write(buffer);
-
+        // 2.写数据
+        ctx.channel().writeAndFlush(buffer);
     }
 
     private ByteBuf getByteBuf(ChannelHandlerContext ctx) {
-        // 1. 获取二进制抽象 ByteBuf
+        byte[] bytes = "你好，闪电侠!".getBytes(Charset.forName("utf-8"));
+
         ByteBuf buffer = ctx.alloc().buffer();
 
-        // 2. 准备数据，指定字符串的字符集为utf-8
-        byte[] bytes = "你好，我来自客户端".getBytes(Charset.forName("utf-8"));
-
-        // 3. 填充数据到ByteBuf
         buffer.writeBytes(bytes);
 
         return buffer;
+    }
+
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf byteBuf = (ByteBuf) msg;
+
+        System.out.println(new Date() + ": 客户端读到数据 -> " + byteBuf.toString(Charset.forName("utf-8")));
     }
 }
--- netty/src/main/java/com/example/netty/server/NettyServer.java
+++ netty/src/main/java/com/example/netty/server/NettyServer.java
@@ -1,64 +1,43 @@
 package com.example.netty.server;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.util.AttributeKey;
-import io.netty.util.concurrent.Future;
-import io.netty.util.concurrent.GenericFutureListener;
 
 public class NettyServer {
 
-    private static final int BEGIN_PORT = 8000;
+    private static final int PORT = 8000;
 
     public static void main(String[] args) {
-
-        NioEventLoopGroup bossGroup = new NioEventLoopGroup(); // 线程组，负责接收连接
-        NioEventLoopGroup workerGroup = new NioEventLoopGroup(); // 线程组，负责读取连接中的数据
+        NioEventLoopGroup boosGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
 
         final ServerBootstrap serverBootstrap = new ServerBootstrap();
-        final AttributeKey<Object> clientKey = AttributeKey.newInstance("clientKey");
-
-        // 一般来说带child的属性为客户端这边的连接属性，不带child的属性为服务端这边的channel属性
         serverBootstrap
-                .group(bossGroup, workerGroup)
+                .group(boosGroup, workerGroup)
                 .channel(NioServerSocketChannel.class)
-//                .handler(new ChannelInboundHandlerAdapter() {
-//                    @Override
-//                    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-//                        super.channelActive(ctx);
-//                    }
-//                })
-//                .attr(AttributeKey.newInstance("serverName"), "nettyServer")
-//                .childAttr(AttributeKey.newInstance("clientKey"), "clientValue")
                 .option(ChannelOption.SO_BACKLOG, 1024)
-                .childOption(ChannelOption.SO_KEEPALIVE, true) // 是否开启TCP底层心跳机制，true为开启
-                .childOption(ChannelOption.TCP_NODELAY, true) // true表示关闭，如果要求高实时性就关闭
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                .childOption(ChannelOption.TCP_NODELAY, true)
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
-                    @Override
-                    protected void initChannel(NioSocketChannel nioSocketChannel) throws Exception {
-                        nioSocketChannel.pipeline().addLast(new FirstServerHandler());
+                    protected void initChannel(NioSocketChannel ch) {
+                        ch.pipeline().addLast(new FirstServerHandler());
                     }
                 });
-        bind(serverBootstrap, BEGIN_PORT);
+
+
+        bind(serverBootstrap, PORT);
     }
 
-    // extract了一个自增绑定端口的方法，即端口如果绑定不成功就增1，直到绑定成功
     private static void bind(final ServerBootstrap serverBootstrap, final int port) {
-        serverBootstrap.bind(port).addListener(new GenericFutureListener<Future<? super Void>>() {
-            @Override
-            public void operationComplete(Future<? super Void> future) throws Exception {
-                if (future.isSuccess()) {
-                    System.out.println("端口[" + port + "]绑定成功");
-                } else {
-                    System.out.println("端口[" + port + "]绑定失败");
-                    bind(serverBootstrap, port + 1);
-                }
+        serverBootstrap.bind(port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("端口[" + port + "]绑定成功!");
+            } else {
+                System.err.println("端口[" + port + "]绑定失败!");
             }
         });
     }
